84e8b04db31da4b197910ddb32cbcf2fe661c269
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2023-01-30 20:31:39 +0300

    revert

--- routes/reparationsRoute.js
+++ routes/reparationsRoute.js
@@ -2,11 +2,6 @@ const { request, response } = require('express');
 const express = require('express');
 const router = express.Router();
 var ObjectId = require('mongoose').Types.ObjectId;
-const cors = require('cors');
-app.use(cors({
-    origin: '*',
-    optionsSuccessStatus: 200
-  }));
 
 
 
