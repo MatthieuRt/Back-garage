f481d6b3348e9b4f62e79033467ae68be5c4e6df
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2023-01-30 20:02:56 +0300

    modification cors

--- app.js
+++ app.js
@@ -12,11 +12,6 @@ const beneficeRoute = require('./routes/beneficeRoute');
 const cors = require('cors');
 app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  }); 
 
 app.use(bodyParser.json());
 
