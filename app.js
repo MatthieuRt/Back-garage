bba17d67f4d0bb522e68bf057278ca7fd38aa473
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2023-01-30 20:15:15 +0300

    revert cors

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-const cors  = require('cors');
-app.use(cors());
-
-app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', 'https://front-garage.vercel.app');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-      res.status(200).end();
-    } else {
-      next();
-    }
-  });
 const express = require('express');
 const app = express();
-
 
 const bodyParser = require('body-parser');
 require('./DbConnexion');
@@ -22,7 +8,8 @@ const reparationsRoute = require('./routes/reparationsRoute');
 const pieceRoute = require('./routes/pieceRoute');
 const demandePaiementRoute = require('./routes/demandePaiementRoute');
 const beneficeRoute = require('./routes/beneficeRoute');
-
+const cors = require('cors');
+app.use(cors());
 
 
 app.use(bodyParser.json());
